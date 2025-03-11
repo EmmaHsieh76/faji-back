7e1cd7e60ad9c2c421b5c97aeb06305c5e3be1e2
EmmaHsieh <z0905631590@gmail.com>
2025-03-11 22:17:06 +0800

    改cors寫法

--- index.js
+++ index.js
@@ -33,17 +33,20 @@ app.use(
         // 網域不允許就發出CORS的錯誤
         callback(new Error('CORS'), false)
       }
-    }
+    },
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
   })
 )
 // 401 => 未授權，我不知道你是誰
 // 403 => FORBIDDEN 禁止，我知道你是誰，但你沒有權限
-app.use((_, req, res, next) => {
-  res.status(StatusCodes.FORBIDDEN).json({
-    success: false,
-    message: '請求被拒絕'
-  })
-})
+// app.use((_, req, res, next) => {
+//   res.status(StatusCodes.FORBIDDEN).json({
+//     success: false,
+//     message: '請求被拒絕'
+//   })
+// })
 
 app.use(express.json())
 app.use((_, req, res, next) => {
